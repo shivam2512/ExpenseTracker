06ef534a19c277fc8a8bdb09062f756ecae94abf
Shivam Shinde <shivamshinde786@gmail.com>
2023-07-09 23:29:39 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ExpenseItem from './components/ExpenseItem';
+function App() {
+  return (
+    <div className="App">
+      <h2>Let's Get Started</h2>
+     <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,7 @@
+function ExpenseItem() {
+    return (<div><h2>Expense Item!</h2>
+    <h4>Food Rs 10</h4>
+    <h4>Petrol Rs 100</h4>
+    <h4>Movies Rs 200</h4></div>);
+}
+export default ExpenseItem;
