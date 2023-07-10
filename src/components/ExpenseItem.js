3fc93898c5e4214eaa65717f83093f93901617e5
Shivam Shinde <shivamshinde786@gmail.com>
2023-07-10 22:35:54 +0530

    ExpenseDate and ExpenseDetails in ExpenseItem function

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,27 @@
 import './ExpenseItem.css';
-
+//ExpenseDate and ExpenseDetails in ExpenseItem function
 function ExpenseItem(props) {
+    const month=props.date.toLocaleString('en-US',{ month : 'long'});
+    const day=props.date.toLocaleString('en-US',{ day : '2-digit'});
+    const year=props.date.getFullYear();
+    const title=props.title;
+    const amount=props.amount;
+    const loc=props.loc;
     return (
     <div className="expense-item">
-    <div>{props.date.toISOString()}
-    
-   
+    <div>
+    <div>{month}</div>
+    <div>{day}</div>
+    <div>{year}</div>
     </div>
     
     <div  className="expense-item__description">
-    <h2>{props.title}</h2>
-    <h2>{props.loc}</h2>
-    <div className="expense-item__price">{props.amount}</div>
+    <h2>{title}</h2>
+    <h2>{loc}</h2>
+    <div className="expense-item__price">{amount}</div>
     </div>
     </div>);
+
+
 }
 export default ExpenseItem;
