0b3529f6e0106a3148c78db417d53437b45eb2e5
Shivam Shinde <shivamshinde786@gmail.com>
2023-07-12 22:46:35 +0530

    Children Props and Behind the Scenes

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,27 +1,17 @@
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 import './ExpenseItem.css';
-//ExpenseDate and ExpenseDetails in ExpenseItem function
+
 function ExpenseItem(props) {
-    const month=props.date.toLocaleString('en-US',{ month : 'long'});
-    const day=props.date.toLocaleString('en-US',{ day : '2-digit'});
-    const year=props.date.getFullYear();
-    const title=props.title;
-    const amount=props.amount;
-    const loc=props.loc;
-    return (
-    <div className="expense-item">
-    <div>
-    <div>{month}</div>
-    <div>{day}</div>
-    <div>{year}</div>
-    </div>
-    
-    <div  className="expense-item__description">
-    <h2>{title}</h2>
-    <h2>{loc}</h2>
-    <div className="expense-item__price">{amount}</div>
-    </div>
-    </div>);
-
-
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+    </Card>
+  );
 }
+
 export default ExpenseItem;
