a1cfc947e8ec8a97fac081adfd6957e5be6d7305
Shivam Shinde <shivamshinde786@gmail.com>
2023-07-20 20:47:08 +0530

    useState - Title and Expense Update

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  
+  const [title, setTitle] = useState(props.title);
+  const [expense, setExpense] = useState(props.amount);
   const clickHandler = () =>
   {
-    console.log("Clicked !!");
+    setTitle('Updated !!');
+   console.log(title);
   };
-  const DeleteHandler = () =>
+  const ExpenseHandler = (props) =>
   {
-    console.log("Delete Clicked !!");
+    setExpense('100');
+    console.log(expense);
   };
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${expense}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
-      <button onClick={DeleteHandler}>Delete Expense</button>
+      <button onClick={ExpenseHandler}>Expense</button>
     </Card>
    );
 }
